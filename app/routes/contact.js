8c365f544eea4b35ef2ef3e7934d831197a07ea9
guilleBFG <guillewest@gmail.com>
2016-10-21 19:51:11 -0300

    fix validation bug
    fix bug

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -9,11 +9,18 @@ export default Ember.Route.extend({
 
   actions:  {
     saveContact(newContact){
-      newContact.save().then( () => {
-         this.controller.set('emailAddress', '');
-         this.controller.set('message','');
-         this.controller.set('responseMessage', true);
-      });
+      let model = this.controller.get('model');
+
+      if(model.get('isValid')){
+        newContact.save().then( () => {
+           this.controller.set('emailAddress', '');
+           this.controller.set('message','');
+           this.controller.set('responseMessage', true);
+        });
+        this.controller.set('errorMessage', false);
+      }else{
+        this.controller.set('errorMessage', true);
+      }
     },
     willTransition() {
       let model = this.controller.get('model');
