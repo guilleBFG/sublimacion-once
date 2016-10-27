97c942a193acb7fda06ae76265b4f3af9e346a5b
guilleBFG <guillewest@gmail.com>
2016-10-27 19:48:18 -0300

    auth test
    auth test

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   emailAddress: DS.attr('string'),
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,36 @@
 import Ember from 'ember';
+
 export default Ember.Route.extend({
-  
+  model(){
+    return this.store.createRecord('admin');
+  },
+  actions: {
+    logIn() {
+
+      let model = this.controller.get('model');
+
+      var user = this.get('store').query( 'admin', {
+        orderBy : 'emailAddress',
+        equalTo : model.get('emailAddress')
+      });
+      console.log(model.get('emailAddress'));
+      console.log(user.get('emailAddress'));
+      console.log(user.get('password'));
+    },
+    registerUser(newUser){
+      newUser.save().then( () => {
+        this.controller.set('emailAddress' , '');
+        this.controller.set('password' , '');
+
+      });
+    },
+    willTransition() {
+      let model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
+
+    }
+  }
 });
